750c8cab8f58edeaad87bd513f86eb6e34354f36
ledayamilee <lyhurtado@externo.cnic.es>
2014-10-25 20:53:36 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,15 @@
+localfilepath = "./UCI HAR Dataset.zip"
+# Check to see if the zip file is there. If it is then don't download it.
+if (file.exists(localfilepath)){
+  warning("Zip file exists in working directory.")
+} else {
+  fileurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileurl, "./UCI HAR Dataset.zip")
+}
+# Print file time
+file.info(localfilepath)$ctime
+
+
 #1. Merges the training and the test sets to create one data set.
 
 
